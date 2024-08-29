1517aa7f61be5324dc0e33588d97952d82b1f36a
ysuzuki19 <ysuzuki.cpp@gmail.com>
2024-08-30 03:38:29 +0900

    add sample

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-use rust_chibi_jwt::Jwt;
+use rust_chibi_jwt::{Alg, Jwt};
 
 // Define payload as sample
 #[derive(Debug, Serialize, Deserialize)]
@@ -13,16 +13,30 @@ pub struct Payload {
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // dummy token with secret "my-secret"
     let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.EpM5XBzTJZ4J8AfoJEcJrjth8pfH28LWdjLo90sYb9g";
-    println!("Input:  {}", token);
-    let mut jwt = Jwt::<Payload>::try_from(token)?;
-    println!("{:?}", jwt);
-
     let secret: &[u8; 9] = b"my-secret";
-    assert!(jwt.verify(secret)?);
-    jwt.sign(secret)?;
 
-    assert!(jwt.verify(secret)?);
-    let reencoded: String = jwt.try_into()?;
-    println!("Output: {}", reencoded);
+    {
+        // Decode and Encode
+        let jwt = Jwt::<Payload>::try_from(token)?;
+
+        assert!(jwt.verify(secret)?);
+
+        let reencoded: String = jwt.try_into()?;
+        assert_eq!(token, reencoded);
+    }
+
+    {
+        // Sign and Encode
+        let payload = Payload {
+            sub: "1234567890".to_string(),
+            name: "John Doe".to_string(),
+            iat: 1516239022,
+        };
+        let mut jwt = Jwt::new(Alg::HS256, payload);
+        jwt.sign(secret)?;
+        let encoded: String = jwt.try_into()?;
+        assert_eq!(token, encoded);
+    }
+
     Ok(())
 }
