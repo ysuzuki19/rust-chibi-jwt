8122181102dc30e44c148904ec6c5ccb59b2d53d
ysuzuki19 <ysuzuki.cpp@gmail.com>
2024-08-31 03:43:05 +0900

    add tests to algorithm::HS256

--- src/jwt/header/algorithm.rs
+++ src/jwt/header/algorithm.rs
@@ -11,6 +11,7 @@ pub(super) trait Algorithm {
     fn sign(self) -> Vec<u8>;
 }
 
+#[derive(Clone)]
 pub(super) struct HS256(Hmac<Sha256>);
 
 impl Algorithm for HS256 {
@@ -27,5 +28,31 @@ impl Algorithm for HS256 {
 
     fn sign(self) -> Vec<u8> {
         self.0.finalize().into_bytes().to_vec()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::jwt::base64::Base64;
+
+    use super::*;
+
+    #[test]
+    fn sign_verify() -> Result<()> {
+        const VALID_DATA:&str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ";
+        const TAMPERED_DATA : &str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkxIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ";
+        let signature = Base64::decode("mpHl842O7xEZjgQ8CyX8xYLDoEORGVMnAxULkW-u8Ek")?;
+        const SECRET: &[u8; 11] = b"test-secret";
+
+        let hs256 = HS256::init(SECRET, VALID_DATA.into())?;
+        assert!(hs256.verify(&signature));
+
+        let hs256 = HS256::init(SECRET, TAMPERED_DATA.into())?;
+        assert!(!hs256.clone().verify(&signature));
+
+        let hs256 = HS256::init(SECRET, TAMPERED_DATA.into())?;
+        let signature = hs256.clone().sign();
+        assert!(hs256.verify(&signature));
+        Ok(())
     }
 }
